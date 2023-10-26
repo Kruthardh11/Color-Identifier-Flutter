e6b655f23912fc9d64a3e24bba122fe5b0f5da7c
John Doe <tirunaharipandu@gmail.com>
2023-10-26 11:19:16 +0530

    backend added

--- lib/face_recognition.dart
+++ lib/face_recognition.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
+// import 'package:image_picker/image_picker.dart';
 
 class FaceRecognitionScreen extends StatefulWidget {
   @override
@@ -11,24 +10,65 @@ class FaceRecognitionScreen extends StatefulWidget {
 class _FaceRecognitionScreenState extends State<FaceRecognitionScreen> {
   String recognizedName = "Recognized Name";
 
-  Future<void> _recognizeFace() async {
-    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+  final TextEditingController registrationNameController =
+      TextEditingController();
+  final TextEditingController registrationEmailController =
+      TextEditingController();
+  final TextEditingController registrationPasswordController =
+      TextEditingController();
 
-    if (image == null) return;
+  final TextEditingController loginEmailController = TextEditingController();
+  final TextEditingController loginPasswordController = TextEditingController();
 
-    var request = http.MultipartRequest(
-        'POST', Uri.parse('http://127.0.0.1:5000/recognize'));
-    request.files.add(await http.MultipartFile.fromPath('image', image.path));
-
-    var response = await request.send();
+  Future<void> registerUser(String name, String email, String password) async {
+    final url = Uri.parse('http://172.22.16.214:5000/register');
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: <String, String>{
+        'name': name,
+        'email': email,
+        'password': password,
+      },
+    );
 
     if (response.statusCode == 200) {
-      final responseJson = json.decode(await response.stream.bytesToString());
-      setState(() {
-        recognizedName = responseJson['name'];
-      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration successful!')),
+      );
+      print('Registration successful: ${response.body}');
     } else {
-      print("Failed to recognize face. Status code: ${response.statusCode}");
+      print('Failed to register: ${response.body}');
+    }
+  }
+
+  Future<void> loginUser(
+      String email, String password, BuildContext context) async {
+    final url = Uri.parse('http://172.22.16.214:5000/login');
+
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: <String, String>{
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (response.statusCode == 200 && response.body == 'LOGGED IN') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login successful!')),
+      );
+      print('Login successful: ${response.body}');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed!')),
+      );
+      print('Login failed: ${response.body}');
     }
   }
 
@@ -38,20 +78,62 @@ class _FaceRecognitionScreenState extends State<FaceRecognitionScreen> {
       appBar: AppBar(
         title: Text('Face Recognition'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            recognizedName != "Recognized Name"
-                ? Text("Recognized Name: $recognizedName")
-                : Container(),
-            ElevatedButton(
-              onPressed: _recognizeFace,
-              child: Text('Capture and Recognize Face'),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Registration Form
+                  TextField(
+                    controller: registrationNameController,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  TextField(
+                    controller: registrationEmailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextField(
+                    controller: registrationPasswordController,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      registerUser(
+                        registrationNameController.text,
+                        registrationEmailController.text,
+                        registrationPasswordController.text,
+                      );
+                    },
+                    child: Text('Register'),
+                  ),
+                  SizedBox(height: 30),
+                  // Login Form
+                  TextField(
+                    controller: loginEmailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextField(
+                    controller: loginPasswordController,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      loginUser(
+                        loginEmailController.text,
+                        loginPasswordController.text,
+                        context,
+                      );
+                    },
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
